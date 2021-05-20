d8dbdc71e2db4429c6243e26a37e2bd48654988a
whiteNgreen <33910273+whiteNgreen@users.noreply.github.com>
2021-05-21 01:15:46 +0200

    Game instance
    will now remember options sliders for volume across game

---
+++ Source/CubeLord/PuzzleMansionGameInstance.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzleMansionGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBELORD_API UPuzzleMansionGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio", meta = (AllowPrivateAccess = "true"))
+	float MasterVolume{ 1.0f };
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio", meta = (AllowPrivateAccess = "true"))
+	float EffectsVolume{ 1.0f };
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio", meta = (AllowPrivateAccess = "true"))
+	float MusicVolume{ 0.5f };
+};
